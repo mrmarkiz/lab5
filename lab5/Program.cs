0ddb332a1cc72b735d3f5b8ab987c5b41cb14511
mrmarkiz <maxtomusiak315@gmail.com>
2023-04-06 17:59:57 +0300

    Add "main" code

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace lab5
+﻿using System.Xml.Serialization;
+
+namespace lab5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int choice;
+            do
+            {
+                Console.Write("Enter task to run(1, 2, 3, 4, 0 - to exit): ");
+                int.TryParse(Console.ReadLine(), out choice);
+                switch (choice)
+                {
+                    case 1:
+                        Task1.Run();
+                        break;
+                    case 2:
+                        Task2.Run();
+                        break;
+                    case 3:
+                        Task3.Run();
+                        break;
+                    case 4:
+                        Task4.Run();
+                        break;
+                }
+
+            } while (choice != 0);
         }
     }
 }
